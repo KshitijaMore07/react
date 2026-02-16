90412a183963c575046947a8ed3f3369f3ce8dfd
Kshitija More <ksmore346@gmail.com>
2026-02-16 23:36:46 +0530

    Create Model.js

---
+++ Model.js
@@ -0,0 +1,14 @@
+class StudentModel {
+
+  getStudent() {
+    // Normally API call here
+    return {
+      id: 1,
+      name: "Kshitija",
+      marks: 95
+    };
+  }
+
+}
+
+export default StudentModel;
