ac2855e111b77e7d1f8e93e37e50d1a65a377601
Kshitija More <ksmore346@gmail.com>
2026-03-06 10:52:30 +0530

    Update Validation App.js

--- Validation App.js
+++ Validation App.js
@@ -1,4 +1,4 @@
 return (
-    <UserProfile title="Student Registration" name="Himanshu" age="21" isAdmin={false} />
+    <UserProfile title="Student Registration" name="kshitija" age="21" isAdmin={false} />
   );
 App.js made he for Validation program
