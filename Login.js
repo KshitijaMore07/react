f3928996cf44cf8523e02b0a1de84da048f476a3
Kshitija More <ksmore346@gmail.com>
2026-03-08 23:46:40 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2>PharmaAgent Login</h2>
+
+      <input type="text" placeholder="Username" /><br/><br/>
+      <input type="password" placeholder="Password" /><br/><br/>
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
