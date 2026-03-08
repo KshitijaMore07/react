575074f83c573b0d4d0f5cb07029ba9a30ac4a9b
Kshitija More <ksmore346@gmail.com>
2026-03-08 23:48:01 +0530

    Create Notification.js

---
+++ Notification.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notifications() {
+  return (
+    <div>
+
+      <h2>Alert Notifications</h2>
+
+      <p>Low stock and expiry alerts.</p>
+
+    </div>
+  );
+}
+
+export default Notifications;
