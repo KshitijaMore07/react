24813238654b7aef858eb8856d2fad9baa18fb82
Kshitija More <ksmore346@gmail.com>
2026-03-08 23:47:36 +0530

    Create Expiry.js

---
+++ Expiry.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Expiry() {
+  return (
+    <div>
+
+      <h2>Expiry Monitoring</h2>
+
+      <p>Medicines nearing expiry will appear here.</p>
+
+    </div>
+  );
+}
+
+export default Expiry;
