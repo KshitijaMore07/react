a06dacb5719b745151c5f218bf84c694122bbdca
Kshitija More <ksmore346@gmail.com>
2026-04-29 21:11:08 +0530

    Create Project.js

---
+++ Project.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section id="projects" className="section">
+      <h2>Projects</h2>
+
+      <div className="project">
+        <h3>Poultry Farm Management System</h3>
+        <p>Built using PHP & PostgreSQL</p>
+      </div>
+
+      <div className="project">
+        <h3>Machine Learning Model</h3>
+        <p>Prediction using Python</p>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
