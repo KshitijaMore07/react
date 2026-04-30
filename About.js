35f9b558eb14531792a88ea91e4d1437c36530b1
Kshitija More <ksmore346@gmail.com>
2026-04-30 16:35:55 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about" className="section">
+      <h2>About Me</h2>
+      <p>
+        I am pursuing MSc Computer Science and interested in Data Analysis,
+        Machine Learning and Software Development.
+      </p>
+    </section>
+  );
+}
+
+export default About;
