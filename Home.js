2dc64d6c23436ba664234281eabdc8f54e2ac19b
Kshitija More <ksmore346@gmail.com>
2026-05-01 23:08:34 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section id="home" className="section">
+      <h1>Hello, I'm Kshitija 👋</h1>
+      <p>Data Science & Machine Learning Enthusiast</p>
+    </section>
+  );
+}
+
+export default Home;
